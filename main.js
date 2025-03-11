75ff20c8a7c86ce6d4a464fa4a05413ecf846698
gweseloh <greg.weseloh@gmail.com>
2025-03-11 14:44:38 -0500

    Created and populated 2D arrays

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+let allScores = [
+    [50, 70, 90],
+    [60, 80, 100],
+    [80, 50, 99]
+];
 
+let theaterSeating = [
+    ["A1", "A2", "A3", "A4"],
+    ["B1", "B2", "B3", "B4"],
+    ["C1", "C2", "C3", "C4"],
+];
+
+let contactsList = [
+    ["Dean", "dean@gmail.com", 1234567890],
+    ["Ted", "ted@outlook.com", 9876543210]
+];
